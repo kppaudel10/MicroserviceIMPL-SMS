1932c803eafacc4fef213890bbe6feda14cb68ef
Kul Paudel <kppaudel56@gmail.com>
2024-10-21 12:25:19 +0545

    feat(ms-setup): initial project setup

---
+++ src/main/java/com/kul/microserviceimplsms/MicroserviceImplSmsApplication.java
@@ -0,0 +1,13 @@
+package com.kul.microserviceimplsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceImplSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceImplSmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kul/microserviceimplsms/MicroserviceImplSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kul.microserviceimplsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceImplSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
